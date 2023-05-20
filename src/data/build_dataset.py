d87fdb27cc4ca7e08c22d29e9a9372582a142f21
omarsayed7 <sayedomar74@gmail.com>
2023-05-20 16:59:03 +0200

    Support sampling from the dataset

--- src/data/build_dataset.py
+++ src/data/build_dataset.py
@@ -1,7 +1,8 @@
-import os, sys, time, json
+import os, sys, time, json, argparse
 from pathlib import Path 
 from tqdm import tqdm 
 import pandas as pd 
+from collections import Counter
 
 main_data_dir = Path("../../data/classification_data")
 images_dir = Path(main_data_dir / "img")
@@ -9,9 +10,7 @@ raw_annotations_dir = Path(main_data_dir / "raw_annotations")
 raw_annotations_file = Path(raw_annotations_dir / "list_category_img.txt")
 
 
-SEED = 42
-
-def build_data(raw_annotations_path: Path, save_path:Path):
+def build_data(raw_annotations_path: Path, save_path: Path, max_num_images: int, sampling_threshold: int):
     print("[INFO] Starting to build fashion data")
     img_path_list = []
     labels_id_list = []
@@ -30,21 +29,59 @@ def build_data(raw_annotations_path: Path, save_path:Path):
             if label_id == 48:
                 label_id = 45
             label_str = image_path.split("/")[1].split("_")[-1].strip()
-            label_mapping[label_id] = label_str
             img_path_list.append(image_path)
-            labels_id_list.append(label_id)
+            labels_id_list.append((label_id - 1))
             labels_str_list.append(label_str)
 
-    assert len(img_path_list) == len(labels_id_list) == len(labels_str_list)
-    fashion_df = pd.DataFrame(columns=["img_path", "label_id", "label_str"])
-    fashion_df["img_path"] = img_path_list
-    fashion_df["label_id"] = labels_id_list
-    fashion_df["label_str"] = labels_str_list
+    class_distribution = dict(Counter(labels_id_list))
+    sampled_classes = []
+    for key, value in class_distribution.items():
+        if value > sampling_threshold:
+            sampled_classes.append(key)
+    print(f"[INFO] Number of sampled classes {len(sampled_classes)}")
+    sampled_img_path_list = []
+    sampled_labels_id_list = []
+    sampled_labels_str_list = []
+
+    #sample from every class number of images 
+    classes_num_images = {}
+    for label_id in set(labels_id_list):
+        classes_num_images[label_id] = 0 
     
+    for i in range(len(labels_id_list)):
+        label_id = labels_id_list[i]
+        if label_id in sampled_classes: 
+            if classes_num_images[label_id] < max_num_images:
+                sampled_img_path_list.append(img_path_list[i])
+                sampled_labels_id_list.append(labels_id_list[i])
+                sampled_labels_str_list.append(labels_str_list[i])
+                label_mapping[label_id] = labels_str_list[i]
+                classes_num_images[label_id] += 1
+    print(f"[INFO] Distribution of the sampled classes {Counter(sampled_labels_str_list)}")
+    print(f"[INFO] Total number of images {len(sampled_img_path_list)}")
+    assert len(sampled_img_path_list) == len(sampled_labels_id_list) == len(sampled_labels_str_list)
+
+    fashion_df = pd.DataFrame(columns=["img_path", "label_id", "label_str"])
+    fashion_df["img_path"] = sampled_img_path_list
+    fashion_df["label_id"] = sampled_labels_id_list
+    fashion_df["label_str"] = sampled_labels_str_list
     fashion_df.to_csv(f"{save_path}/fashion_data.csv", index=False)
+
     with open(f'{save_path}/mapping.json', 'w') as fp:
         json.dump(label_mapping, fp)
+
     print("[INFO] Finished parsing the fashion dataset and saved to {dataframe_path}/fashion_data.csv")
 
-build_data(raw_annotations_file, main_data_dir)
+
+
+if __name__ == "__main__":
+    '''
+    Main function, used to parse the arguments and call the main function
+    '''
+    parser = argparse.ArgumentParser(description="Configuration of setup and training process")
+    parser.add_argument('-maximum_num_images', '--maximum_num_images', type= int, help= '', default = 1000)
+    parser.add_argument('-sampling_threshold', '--sampling_threshold', type= int, help= '', default = 500)
+    args = parser.parse_args()
+
+    build_data(raw_annotations_file, main_data_dir, args.maximum_num_images, args.sampling_threshold)
     
